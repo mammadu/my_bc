66d946fbc5445d4172b1cbfdebb5618aa67b36e3
jairo guzman <guzman_j@qwasar.io>
2021-07-18 02:29:47 +0000

    Counting all tokens Tokens are being counted, space included, consecutive numbers are counted as one token

--- lexer.c
+++ lexer.c
@@ -1,19 +1,40 @@
 #include "lexer.h"
 
+
+int my_isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
+}
+
 //returns amount of tokens in string
     //use to allocate memory in a char** later on
 int tokens_counter(char* source)
 {
-    int i = 0; 
     int token_amount = 0;
+    int nonconsecutive_number = 0;
     
-    while (source[i] != '\0')
+    for (int i =0; source[i] != '\0'; i += 1)
     {
-                
-        i += 1;
+        if(my_isdigit(source[i]) && nonconsecutive_number == 0)
+        {
+            nonconsecutive_number = 1;
+            token_amount += 1;
+        } 
+        else if (my_isdigit(source[i]) && nonconsecutive_number == 1)
+            continue;
+        else
+        {
+            nonconsecutive_number = 0;
+            token_amount += 1;
+        }
     }
-
     return token_amount;
 }
 
+
+int main ()
+{
+    printf("%d", tokens_counter("1122+2*(3-424)/5"));
+    return 0;
+}
 //Starting up branch
