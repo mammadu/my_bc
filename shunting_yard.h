af51022a87a83a690db3cefa6761599a60c80ce9
mammadu diallo <diallo_m@qwasar.io>
2021-08-07 02:49:28 +0000

    Created rough template of how to do shunting yard algorithm

--- shunting_yard.h
+++ shunting_yard.h
@@ -4,7 +4,13 @@
 #include "my_c_functions.h"
 
 typedef struct shunting_yard {
-  
+    //malloc output_queue as same size as token_size.
+    //push index of values and proper operators to output queue
+    //last character is null
+    int* output_queue;
+    int* operator_stack;
+
+    //use output_queue and operator_stack to create RPN
 } shunting_yard;
 
 
