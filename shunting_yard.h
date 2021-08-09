10d354b5002596329e7c86654fd34ec65d491fc0
jairo guzman <guzman_j@qwasar.io>
2021-08-09 23:49:39 +0000

    Added a int and char pointer pointer for ease of manipulation

--- shunting_yard.h
+++ shunting_yard.h
@@ -7,8 +7,8 @@ typedef struct shunting_yard {
     //malloc output_queue as same size as token_size.
     //push index of values and proper operators to output queue
     //last character is null
-    int* output_queue;
-    int* operator_stack;
+    int** output_queue;
+    char** operator_stack;
 
     //use output_queue and operator_stack to create RPN
 } shunting_yard;
