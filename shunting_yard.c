610d6ef59d923cd9faa7b6e19d41abb6d2c74ae5
jairo guzman <guzman_j@qwasar.io>
2021-08-10 00:12:20 +0000

    Adding more shape to the foundations of the Shunting yard algorithm

---
+++ shunting_yard.c
@@ -0,0 +1,11 @@
+#include "shunting_yard.h"
+
+//main function returns a rpn of the tokenized input
+shunting_yard* my_rpn(tokens* tokens)
+{
+    for(int i = 0; i < token_count; i ++)
+    {
+        if (my_isdigit(tokens->tokens[i][FIRST_CHAR]))
+        
+    }
+}
--- shunting_yard.h
+++ shunting_yard.h
@@ -2,6 +2,7 @@
 #define shunting_yard_H
 
 #include "my_c_functions.h"
+#include "lexer.h"
 
 typedef struct shunting_yard {
     //malloc output_queue as same size as token_size.
@@ -9,6 +10,7 @@ typedef struct shunting_yard {
     //last character is null
     int** output_queue;
     char** operator_stack;
+    int output_queue_count;
 
     //use output_queue and operator_stack to create RPN
 } shunting_yard;
