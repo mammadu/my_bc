cf69ef639dadbc1496192694e7eca4f8540f9419
jairo guzman <guzman_j@qwasar.io>
2021-08-10 00:30:34 +0000

    Allocated memory for data type used for algo implementation

--- shunting_yard.c
+++ shunting_yard.c
@@ -4,14 +4,15 @@
 shunting_yard* my_rpn(tokens* tokens)
 {
     shunting_yard* syd = malloc(sizeof(shunting_yard));
-
+    syd->output_queue = malloc(sizeof(char*) * tokens->token_count); 
+    syd->operator_stack = malloc(sizeof(char*) * tokens->token_count);
+    
     for(int i = 0; i < tokens->token_count; i ++)
     {
-        if (my_isdigit(tokens->tokens[i][FIRST_CHAR]))
-        { 
-            syd->output_queue = my_strdup(tokens->tokens[i]);
+        if (my_isdigit(tokens->tokens[i][FIRST_CHAR])) { 
+            syd->output_queue[syd->output_queue_count] = my_strdup(tokens->tokens[i]);
             syd->output_queue_count += 1;
-        }
+        } else if ()
         
     }
 }
